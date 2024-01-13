1bf2623b0e1a9ca8df2749ee9515bd91171fb5eb
Luisa Chiu <luisachiu1@gmail.com>
2024-01-13 01:08:57 -0800

    updated data.js

--- index.js
+++ index.js
@@ -452,8 +452,8 @@ import { URLs } from './user-data/urls.js';
   
   populateProjects(ballBalancing, "ball-balancing");
   populateProjects(penPlotter, "pen-plotter");
-  populateProjects(simulationProjects, "simulation-projects");
-  populateProjects(hapticsProjects, "haptics-projects");
+  // populateProjects(simulationProjects, "simulation-projects");
+  // populateProjects(hapticsProjects, "haptics-projects");
 
   populateExp_Edu(experience, "experience");
 
