e305b25ddb32e5d99a1f8f7d759a4f1a2b57d094
Luisa Chiu <luisachiu1@gmail.com>
2024-01-21 15:55:48 -0800

    fixed bug in project links

--- data.js
+++ data.js
@@ -104,7 +104,7 @@ export const projects = {
                 {
                     text: "Powerpoint Slides",
                     link: "https://docs.google.com/presentation/d/1zU3avEw_QoErOw8UOeQKcBMYGLnliPxZZhomeFnZxes/edit#slide=id.p1",
-                }
+                },
                 {
                     text: "Documentation and Reports",
                     link: "ME305_Portfolio/index.html",
