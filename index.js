313232f84b850a3a8c6e8930a7ddcf7669622cde
Luisa Chiu <luisachiu1@gmail.com>
2024-01-20 22:52:00 -0800

    attempt to fix dynamic animation

--- index.js
+++ index.js
@@ -154,8 +154,8 @@ import { URLs } from './user-data/urls.js';
   
     let img = document.createElement("img");
     // img.className = "img-fluid";
-    img.style.width = "200px";
-    img.style.height = "200px";
+    img.style.width = "150px";
+    img.style.height = "150px";
     img.style.float = "left";
   
     let divResumeContentLeft = document.createElement("div");
